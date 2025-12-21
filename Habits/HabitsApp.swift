1e4e0deb95ca371853c2ab3687fa0326ad2c7340
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2025-12-21 11:56:48 +0300

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Andrey on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
