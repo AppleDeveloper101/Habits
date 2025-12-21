c201ed3b926a36d7ad92495add258eac97eda1f3
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2025-12-21 16:49:19 +0300

    Add dimensionsOverlay view modifier
    Debugging modifier for displaying view size on screen

---
+++ Habits/Extensions/View+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  View+Extensions.swift
+//  Habits
+//
+//  Created by Andrey on 21/12/2025.
+//
+
+import SwiftUI
+
+extension View {
+    func dimensionsOverlay(fontSize: CGFloat = 16, displayedInside: Bool = false) -> some View {
+        self.overlay {
+            GeometryReader { proxy in
+                Text("\(proxy.size.width.description) \(proxy.size.height.description)")
+                    .offset(y: displayedInside ? 0 : proxy.size.height + 4)
+                    .font(.system(size: fontSize, weight: .thin))
+                    .foregroundStyle(.pink)
+                    .monospaced()
+                    .fixedSize()
+            }
+        }
+    }
+}
