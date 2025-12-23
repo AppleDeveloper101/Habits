97a66cddfaad25bbfff9e021f24b33272555810b
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2025-12-23 18:11:33 +0300

    Add CardHabitCalendar view

---
+++ Habits/Views/CardHabitCalendar.swift
@@ -0,0 +1,61 @@
+//
+//  CardHabitCalendar.swift
+//  Habits
+//
+//  Created by Andrey on 23/12/2025.
+//
+
+import SwiftUI
+
+struct CardHabitCalendar: View {
+    private let displayedMonths: [Date]
+    private let habit: Habit
+    
+    private let columnToGridSpacing: CGFloat = 2
+    private let gridToGridSpacing: CGFloat = 8
+    
+    init(displayedMonths: [Date], habit: Habit) {
+        self.displayedMonths = displayedMonths
+        self.habit = habit
+    }
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: columnToGridSpacing) {
+            CardWeekdaysColumn()
+            
+            HStack(spacing: gridToGridSpacing) {
+                ForEach(displayedMonths, id: \.self) { monthDate in
+                    CardMonthGrid(date: monthDate, habit: habit)
+                }
+            }
+        }
+    }
+}
+
+#if DEBUG
+
+import Foundation
+import SwiftData
+
+#Preview {
+    // TODO: Implement adaptive algorithm (pass from HabitCard?)
+    let calendar = Calendar.current
+    var displayedMonths: [Date] {
+        guard let startOfFirstDisplayedMonth = calendar.dateInterval(of: .month, for: .now)?.start else {
+            fatalError("Failed to retrieve startOfFirstShownMonth in CardHabitCalendar")
+        }
+        
+        return (-1..<3).map { offset in
+            guard let date = calendar.date(byAdding: .month, value: offset, to: startOfFirstDisplayedMonth) else {
+                fatalError("Failed to retrieve date in CardHabitCalendar")
+            }
+            return date
+        }
+    }
+    
+    CardHabitCalendar(displayedMonths: displayedMonths, habit: SampleData.shared.sampleHabit)
+        .modelContainer(SampleData.shared.container)
+        .containerRelativeFrame(.horizontal, alignment: .leading)
+        .padding(.leading)
+}
+#endif
