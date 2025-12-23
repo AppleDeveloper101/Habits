c1ce54a497c49d6a2391faa6f8fa800409d0e562
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2025-12-23 11:32:15 +0300

    Add CardPaddingCell view

---
+++ Habits/Views/CardPaddingCell.swift
@@ -0,0 +1,19 @@
+//
+//  CardPaddingCell.swift
+//  Habits
+//
+//  Created by Andrey on 22/12/2025.
+//
+
+import SwiftUI
+
+struct CardPaddingCell: View {
+    var body: some View {
+        Color.clear
+            .frame(width: 16, height: 16)
+    }
+}
+
+#Preview {
+    CardPaddingCell()
+}
