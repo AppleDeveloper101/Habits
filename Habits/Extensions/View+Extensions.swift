ac8c26f549d3d8feb996d73feffa01fbd7352bad
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2025-12-27 11:32:32 +0300

    Update dimensionsOverlay with fontColor property

--- Habits/Extensions/View+Extensions.swift
+++ Habits/Extensions/View+Extensions.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 extension View {
-    func dimensionsOverlay(fontSize: CGFloat = 16, displayedInside: Bool = false) -> some View {
+    func dimensionsOverlay(fontColor: Color = .pink, fontSize: CGFloat = 16, displayedInside: Bool = false) -> some View {
         self.overlay {
             GeometryReader { proxy in
                 Text("\(proxy.size.width.description) \(proxy.size.height.description)")
                     .offset(y: displayedInside ? 0 : proxy.size.height + 4)
                     .font(.system(size: fontSize, weight: .thin))
-                    .foregroundStyle(.pink)
+                    .foregroundStyle(fontColor)
                     .monospaced()
                     .fixedSize()
             }
