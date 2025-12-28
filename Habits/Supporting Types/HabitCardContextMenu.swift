fd054ec5cb1eb49366eaf95299d0c17a7945eb7c
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2025-12-28 13:42:50 +0300

    Define HabitCardContextMenu view modifier

---
+++ Habits/Supporting Types/HabitCardContextMenu.swift
@@ -0,0 +1,28 @@
+//
+//  HabitCardContextMenu.swift
+//  Habits
+//
+//  Created by Andrey on 28/12/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HabitCardContextMenu: ViewModifier {
+    @Environment(\.modelContext) private var context
+    
+    let habit: Habit
+    
+    func body(content: Content) -> some View {
+        content
+            .contextMenu {
+                Button("Delete", systemImage: "trash", role: .destructive) {
+                    context.delete(habit)
+                    
+                    if context.hasChanges { do { try context.save() } catch {
+                        fatalError("Failed to save persistent storage after deleting a habit") }
+                    }
+                }
+            }
+    }
+}
