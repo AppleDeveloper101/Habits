f7ea885a8b47e1815abe2918d557443af0d7849e
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2025-12-31 11:23:39 +0300

    Use global constants instead of SampleData class

--- Habits/SwiftData/SampleData.swift
+++ Habits/SwiftData/SampleData.swift
@@ -8,41 +8,35 @@
 import Foundation
 import SwiftData
 
-class SampleData {
-    static let shared = SampleData()
-    
-    let container: ModelContainer
-    let context: ModelContext
-    let sampleHabit: Habit
-    
-    private init() {
-        do {
-            let schema = Schema([
-                Habit.self,
-                Record.self,
-            ])
-            let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
-            
-            container = try ModelContainer(for: schema, configurations: configuration)
-            context = container.mainContext
-            sampleHabit = Habit(title: "Sample Habit", emoji: "🌁")
-            
-            let calendar = Calendar.current
-            let startOfToday = calendar.startOfDay(for: .now)
-            let dayOffsets = [-14, -7, -4, -3, -2, 0]
-            let records = dayOffsets.map { offset in
-                if let date = calendar.date(byAdding: .day, value: offset, to: startOfToday) {
-                    return Record(date: date, habit: sampleHabit)
-                } else {
-                    fatalError("Failed to get date from offset during SampleData initialization")
-                }
+let sampleHabit = Habit(title: "Sample Habit", emoji: "🌁")
+
+let sampleContainer: ModelContainer = {
+    do {
+        let schema = Schema([
+            Habit.self,
+            Record.self,
+        ])
+        let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        
+        let container = try ModelContainer(for: schema, configurations: configuration)
+        let context = container.mainContext
+        
+        let calendar = Calendar.current
+        let startOfToday = calendar.startOfDay(for: .now)
+        let dayOffsets = [-14, -7, -4, -3, -2, 0]
+        let records = dayOffsets.map { offset in
+            if let date = calendar.date(byAdding: .day, value: offset, to: startOfToday) {
+                return Record(date: date, habit: sampleHabit)
+            } else {
+                fatalError("Failed to get date from offset during SampleData initialization")
             }
-            
-            context.insert(sampleHabit)
-            records.forEach { context.insert($0) }
-            try context.save()
-        } catch {
-            fatalError("Error occurred during SampleData initialization: \(error)")
         }
+        
+        context.insert(sampleHabit)
+        records.forEach { context.insert($0) }
+        
+        return container
+    } catch {
+        fatalError("Error occurred during SampleData initialization: \(error)")
     }
-}
+}()
