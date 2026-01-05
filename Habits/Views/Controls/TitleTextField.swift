1a0d8814c375fc2359e54a34b1f577dc5a675f62
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-01-05 21:01:42 +0300

    Add TitleTextField view

---
+++ Habits/Views/Controls/TitleTextField.swift
@@ -0,0 +1,41 @@
+//
+//  TitleTextField.swift
+//  Habits
+//
+//  Created by Andrey on 05/01/2026.
+//
+
+import SwiftUI
+
+struct TitleTextField: View {
+    @Binding private var text: String
+    
+    @FocusState private var isFocused
+    
+    private let prompt = {
+        Text("Title...")
+            .font(.headline)
+            .foregroundStyle(.fillVibrantSecondary)
+    }()
+    
+    init(_ text: Binding<String>) {
+        self._text = text
+    }
+    
+    var body: some View {
+        TextField("Title", text: $text, prompt: prompt)
+            .focused($isFocused)
+            .font(.headline)
+            .foregroundStyle(.labelVibrantPrimary)
+            .padding(.horizontal, 12)
+            .frame(height: 44)
+            .backgroundWithStrokeOverlay(shape: Capsule())
+    }
+}
+
+#Preview {
+    @Previewable @State var text = ""
+    
+    TitleTextField($text)
+        .padding()
+}
