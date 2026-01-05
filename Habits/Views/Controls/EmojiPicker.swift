935b4ee1a9b20aeaf0a49dcf496392e1b1b17e59
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-01-05 14:08:42 +0300

    Create EmojiPicker

---
+++ Habits/Views/Controls/EmojiPicker.swift
@@ -0,0 +1,48 @@
+//
+//  EmojiPicker.swift
+//  Habits
+//
+//  Created by Andrey on 05/01/2026.
+//
+
+import SwiftUI
+
+struct EmojiPicker: View {
+    @Binding private var emoji: String
+    
+    @FocusState private var isFocused
+    
+    init(_ emoji: Binding<String>) {
+        self._emoji = emoji
+    }
+    
+    var body: some View {
+        ShapeWithStroke(Circle())
+            .overlay(alignment: .center) {
+                TextField("", text: $emoji)
+                    .focused($isFocused)
+                    .keyboardType(UIKeyboardType(rawValue: 124)!)
+                    .multilineTextAlignment(.center)
+                    .containerRelativeFrame(.horizontal)
+                    .onChange(of: emoji) { oldValue, newValue in
+                        emoji = (newValue.first?.description ?? "⚠️")
+                        // FIXME: Prone to passing through more than one symbol
+                    }
+            }
+            .overlay {
+                if emoji.isEmpty && !isFocused {
+                    Image(systemName: "plus")
+                        .foregroundStyle(.fillVibrantSecondary)
+                }
+            }
+            .font(.title2)
+            .frame(width: 44, height: 44)
+            .onTapGesture { isFocused = true }
+    }
+}
+
+#Preview {
+    @Previewable @State var emoji = ""
+    
+    EmojiPicker($emoji)
+}
