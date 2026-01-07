f7ccd7fe3d6adff9ccbe5770cdb35cbcae122931
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-01-07 12:31:33 +0300

    Add SheetPaddingCell view

---
+++ Habits/Views/Primitives/SheetPaddingCell.swift
@@ -0,0 +1,14 @@
+//
+//  SheetPaddingCell.swift
+//  Habits
+//
+//  Created by Andrey on 07/01/2026.
+//
+
+import SwiftUI
+
+struct SheetPaddingCell: View {
+    var body: some View {
+        Color.clear.frame(width: 44, height: 44)
+    }
+}
