0d8cb7b5e6713358258cd11ec149bc615c8c721f
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-01-24 15:45:22 +0300

    Change toolbar inside NewHabitSheet
    Replace system toolbar with custom one

--- Habits/Views/Screens/NewHabitSheet.swift
+++ Habits/Views/Screens/NewHabitSheet.swift
@@ -14,6 +14,7 @@ struct NewHabitSheet: View {
     
     @State private var emoji: String = ""
     @State private var title: String = ""
+    @State private var detentHeight: CGFloat = .zero
     
     private var formattedTitle: String {
         title.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -22,34 +23,87 @@ struct NewHabitSheet: View {
     private let emojiToTextFieldSpacing: CGFloat = 8
     
     var body: some View {
-        VStack {
+        VStack(spacing: .zero) {
+            header()
             HStack(spacing: emojiToTextFieldSpacing) {
                 EmojiPicker($emoji)
                 TitleTextField($title)
             }
-            .padding()
+            .padding(16)
         }
-        .navigationTitle("New Habit")
-        .toolbarTitleDisplayMode(.inline)
-        .presentationDetents([.fraction(0.19)])
+        .setHeight(for: $detentHeight)
         .frame(maxHeight: .infinity, alignment: .top)
-        .toolbar {
-            ToolbarItem(placement: .cancellationAction) {
-                Button("Cancel", systemImage: "xmark", role: .cancel) {
-                    dismiss()
-                }
-                .tint(.labelVibrantPrimary)
-            }
-            ToolbarItem(placement: .confirmationAction) {
-                Button("Done", systemImage: "checkmark") {
-                    let newHabit = Habit(title: formattedTitle, emoji: emoji)
-                    context.insert(newHabit)
-                    dismiss()
-                }
-                .tint(.labelVibrantPrimary)
-                .disabled(formattedTitle.isEmpty)
-            }
+        .presentationDetents([.height(detentHeight)])
+    }
+    
+    private func header() -> some View {
+        HStack(spacing: .zero) {
+            cancelButton()
+            Spacer()
+            headerTitle()
+            Spacer()
+            confirmButton()
         }
+        .padding(.horizontal)
+        .padding(.top, 16)
+        .padding(.bottom, 10)
+    }
+    
+    private func cancelButton() -> some View {
+        Button {
+            dismiss()
+        } label: {
+            Circle()
+                .fill(.quaternary)
+                .frame(width: 44, height: 44)
+                .overlay {
+                    Image(systemName: "xmark")
+                        .font(.title2)
+                        .foregroundStyle(.labelVibrantPrimary)
+                }
+        }
+        .buttonStyle(.plain)
+    }
+    
+    private func headerTitle() -> some View {
+        Text("New Habit")
+            .font(.headline)
+            .foregroundStyle(.labelVibrantPrimary)
+    }
+    
+    private func confirmButton() -> some View {
+        Button {
+            let newHabit = Habit(title: formattedTitle, emoji: emoji)
+            context.insert(newHabit)
+            dismiss()
+        } label: {
+            Circle()
+                .fill(.labelVibrantPrimary)
+                .frame(width: 44, height: 44)
+                .overlay {
+                    Image(systemName: "checkmark")
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                }
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+// Source – CalendarSheet.swift
+private extension View {
+    func setHeight(for property: Binding<CGFloat>) -> some View {
+        self
+            .overlay {
+                GeometryReader { proxy in
+                    Color.clear
+                        .onGeometryChange(for: CGFloat.self) { proxy in
+                            proxy.size.height
+                        } action: { newHeight in
+                            property.wrappedValue = newHeight
+                        }
+                }
+            }
     }
 }
 
@@ -61,8 +115,6 @@ struct NewHabitSheet: View {
             isPresented = true
         }
         .sheet(isPresented: $isPresented) {
-            NavigationStack {
-                NewHabitSheet()
-            }
+            NewHabitSheet()
         }
 }
