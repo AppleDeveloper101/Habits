679a5b8af00927bdf095f3f2122d332a8458384a
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-02-05 14:58:18 +0300

    Move HabitCardContextMenu directly inside HabitCard

--- Habits/Views/Components/HabitCard.swift
+++ Habits/Views/Components/HabitCard.swift
@@ -55,6 +55,31 @@ private func monthsToDisplay() -> [Date] {
     return displayedMonths
 }
 
+struct HabitCardContextMenu: ViewModifier {
+    @Environment(\.modelContext) private var context
+    
+    let habit: Habit
+    
+    func body(content: Content) -> some View {
+        content
+            .contextMenu {
+                Button("Delete", systemImage: "trash", role: .destructive) {
+                    context.delete(habit)
+                    
+                    if context.hasChanges { do { try context.save() } catch {
+                        fatalError("Failed to save persistent storage after deleting a habit") }
+                    }
+                }
+            }
+    }
+}
+
+extension View {
+    func habitCardContextMenu(habit: Habit) -> some View {
+        modifier(HabitCardContextMenu(habit: habit))
+    }
+}
+
 #if DEBUG
 
 import SwiftData
