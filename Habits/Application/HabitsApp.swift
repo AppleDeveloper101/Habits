ac5fe6bf8f1021fa70bb289ecdc288f59eb2ce01
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-04-27 15:59:50 +0300

    Add HabitsApp

---
+++ Habits/Application/HabitsApp.swift
@@ -0,0 +1,16 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Andrey on 27/04/2026.
+//
+
+import SwiftUI
+
+@main struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // TODO: Homepage
+        }
+    }
+}
