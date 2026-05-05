18b667e41a9bffcc4de43844b226516e7f199e77
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-05-05 17:40:45 +0300

    Fix HabitCard shadow glitching using context menu

--- Habits/Features/HabitCard/HabitCard.swift
+++ Habits/Features/HabitCard/HabitCard.swift
@@ -16,19 +16,23 @@ struct HabitCard: View {
     }
     
     var body: some View {
-        VStack(spacing: 8) {
-            header()
-        }
-        .padding(12)
-        .background(defaultStyleShape(RoundedRectangle(cornerRadius: 24), isElevated: true))
-        .contentShape(RoundedRectangle(cornerRadius: 24).inset(by: 12))
-        .contentShape(.contextMenuPreview, RoundedRectangle(cornerRadius: 24))
-        .onTapGesture {
-            SheetManager.shared.present(.habitInfoSheet(habit))
-        }
-        .contextMenu {
-            Button("Delete", systemImage: "trash", role: .destructive) {
-                DataManager.shared.delete(habit)
+        ZStack {
+            defaultStyleShape(RoundedRectangle(cornerRadius: 24), isElevated: true)
+            
+            VStack(spacing: 8) {
+                header()
+            }
+            .padding(12)
+            .background(defaultStyleShape(RoundedRectangle(cornerRadius: 24), isElevated: true, isShadowEnabled: false))
+            .contentShape(RoundedRectangle(cornerRadius: 24).inset(by: 12))
+            .contentShape(.contextMenuPreview, RoundedRectangle(cornerRadius: 24))
+            .onTapGesture {
+                SheetManager.shared.present(.habitInfoSheet(habit))
+            }
+            .contextMenu {
+                Button("Delete", systemImage: "trash", role: .destructive) {
+                    DataManager.shared.delete(habit)
+                }
             }
         }
     }
