a8d64c8a7022e6e2fcae64689fa4b465ecd26a96
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-05-05 16:43:07 +0300

    Add HabitCard

---
+++ Habits/Features/HabitCard/HabitCard.swift
@@ -0,0 +1,51 @@
+//
+//  HabitCard.swift
+//  Habits
+//
+//  Created by Andrey on 04/05/2026.
+//
+
+import SwiftUI
+
+struct HabitCard: View {
+    
+    private let habit: Habit
+    
+    init(_ habit: Habit) {
+        self.habit = habit
+    }
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            header()
+        }
+        .padding(12)
+        .background(defaultStyleShape(RoundedRectangle(cornerRadius: 24), isElevated: true))
+        .contentShape(RoundedRectangle(cornerRadius: 24).inset(by: 12))
+        .onTapGesture {
+            SheetManager.shared.present(.habitInfoSheet(habit))
+        }
+        .contextMenu {
+            Button("Delete", systemImage: "trash", role: .destructive) {
+                DataManager.shared.delete(habit)
+            }
+        }
+    }
+    
+    private func header() -> some View {
+        HStack {
+            HStack(spacing: 4) {
+                Text(habit.emoji)
+                    .frame(width: 38, height: 38)
+                    .font(.system(size: 34, weight: .regular))
+                Text(habit.title)
+                    .font(.headline)
+            }
+            .foregroundStyle(.accent)
+            
+            Spacer()
+            
+            StreakButton(habit: habit)
+        }
+    }
+}
