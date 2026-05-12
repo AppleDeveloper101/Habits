038bd4058f9a723524b3a6cef46f323a13b95b3f
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-05-12 18:11:44 +0300

    Add text transition for title in HabitCard

--- Habits/Features/HabitCard/HabitCard.swift
+++ Habits/Features/HabitCard/HabitCard.swift
@@ -11,8 +11,11 @@ struct HabitCard: View {
     
     private let habit: Habit
     
+    @State private var title: String
+    
     init(_ habit: Habit) {
         self.habit = habit
+        self.title = habit.title
     }
     
     var body: some View {
@@ -30,19 +33,20 @@ struct HabitCard: View {
     private func header() -> some View {
         HStack {
             HStack(spacing: 4) {
-                if !habit.emoji.isEmpty {
-                    Text(habit.emoji)
-                        .frame(width: 38, height: 38)
-                        .font(.system(size: 34, weight: .regular))
-                } else {
-                    Text("🎯")
-                        .saturation(0)
-                        .contrast(1.17)
-                        .frame(width: 38, height: 38)
-                        .font(.system(size: 34, weight: .regular))
-                }
-                Text(habit.title)
+                Text(habit.emoji.isEmpty ? "🎯" : habit.emoji)
+                    .saturation(habit.emoji.isEmpty ? 0 : 1)
+                    .contrast(habit.emoji.isEmpty ? 1.17 : 1)
+                    .frame(width: 38, height: 38)
+                    .font(.system(size: 34, weight: .regular))
+                Text(title)
                     .font(.headline)
+                    .contentTransition(.numericText())
+                    .onChange(of: habit.title) { _, newValue in
+                        Task {
+                            try? await Task.sleep(for: .seconds(0.5))
+                            withAnimation { title = newValue }
+                        }
+                    }
             }
             .foregroundStyle(.accent)
             
