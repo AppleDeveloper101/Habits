23aa1b5500a3c2006363064a5cc8473e4bf82e53
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-05-21 17:35:01 +0300

    Implement intermediate ConfirmationButton logic

--- Habits/Features/HabitInfoSheet/ConfirmationButton.swift
+++ Habits/Features/HabitInfoSheet/ConfirmationButton.swift
@@ -16,6 +16,10 @@ struct ConfirmationButton: View {
         self.action = action
     }
     
+    @State private var progress = -1
+    @State private var incrementationTask: Task<Void, Never>?
+    @State private var disengagementTask: Task<Void, Never>?
+    
     @State private var isCanceled = false
     @GestureState private var isHolding = false
     
@@ -24,17 +28,17 @@ struct ConfirmationButton: View {
     
     var body: some View {
         HStack(spacing: 4) {
-            Image(systemName: "trash.circle.fill")
+            Text(progress == -1 ? "–" : progress.description).contentTransition(.identity) // Image(systemName: "trash.circle.fill")
                 .font(.system(size: 26, weight: .regular))
                 .foregroundStyle(.deleteButtonLabel)
-            Text("isHolding: \(isHolding ? "+" : "-") | isCanceled: \(isCanceled ? "+" : "-")") // MARK: R // Text("Delete")
-                .font(.caption).contentTransition(.identity) // MARK: R // .font(.headline)
+            Text("Delete")
+                .font(.headline)
                 .foregroundStyle(.deleteButtonLabel)
         }
         .frame(height: 44)
         .padding(.leading, 6)
         .padding(.trailing, 8)
-        .frame(maxWidth: isHolding && !isCanceled ? .infinity : nil)
+        .frame(maxWidth: status == .resting ? nil : .infinity)
         .background { DefaultStyleShape(.capsule) }
         .onGeometryChange(for: CGRect.self) { proxy in
             proxy.frame(in: .global)
@@ -49,26 +53,78 @@ struct ConfirmationButton: View {
         .gesture(
             DragGesture(minimumDistance: .zero, coordinateSpace: .global)
                 .updating($isHolding) { drag, isHolding, _ in
-                    isHolding = true
-                    
                     dragLocation = drag.location
                     
                     guard !isCanceled else { return }
                     
-                    if !buttonFrame.insetBy(dx: -22, dy: -22).contains(dragLocation) && isHolding { isCanceled = true }
+                    isHolding = true
                     
-                    guard !isHolding else { return }
+                    if !buttonFrame.insetBy(dx: -22, dy: -22).contains(dragLocation) && isHolding { isCanceled = true }
                 }
                 .onEnded { drag in
                     isCanceled = false
                 }
         )
+        .onChange(of: [isHolding, isCanceled]) { oldValue, newValue in
+            if !isHolding || isCanceled { disengage() }
+            if isHolding && !isCanceled { engage() }
+        }
         
-        .animation(.smooth, value: isHolding)
-        .animation(.smooth, value: isCanceled)
+        .animation(.smooth, value: status) // Rfmrt
+        .animation(.smooth, value: isHolding) // Rfmrt
+        .animation(.smooth, value: isCanceled) // Rfmrt
         
         .onChange(of: isCanceled) { _, newValue in if newValue == true { AudioServicesPlaySystemSound(1100) } } // MARK: DB
         .onChange(of: isHolding) { _, newValue in if newValue == false { AudioServicesPlaySystemSound(1103) } } // MARK: DB
+    }
+}
+
+private extension ConfirmationButton {
+    
+    func engage() {
+        guard incrementationTask?.isCancelled ?? true else { return }
+        
+        disengagementTask?.cancel()
+        
+        incrementationTask = Task {
+            if status == .resting { progress = 0 }
+            else if status == .engaged { progress = 1 }
+        }
+    }
+    
+    func disengage() {
+        guard disengagementTask?.isCancelled ?? true else { return }
+        
+        incrementationTask?.cancel()
+        
+        disengagementTask = Task {
+            progress = 0
+            
+            try? await Task.sleep(for: .seconds(0.5)) // Order matter
+            guard !Task.isCancelled else { return }
+            
+            progress = -1
+        }
+    }
+    
+    enum ButtonState {
+        case resting
+        case engaged
+        case incrementing
+        case executingAction
+        
+        case undefined
+    }
+    
+    var status: ButtonState {
+        switch progress {
+        case ..<0 : return .resting
+        case 0 : return .engaged
+        case 0...3: return .incrementing
+        case 3...: return .executingAction
+            
+        default: return .undefined
+        }
     }
 }
 
