4b7c8c5bf2f7610d546fdc384d4836ff5ff56969
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-06-11 16:25:18 +0300

    Add HabitCard emoji change animation

--- Habits/Features/HabitCard/HabitCard.swift
+++ Habits/Features/HabitCard/HabitCard.swift
@@ -14,10 +14,15 @@ struct HabitCard: View {
     private let habit: Habit
     
     @State private var title: String
+    @State private var emoji: String
+    
+    @State private var emojiScale = 1.0
+    @State private var isAnimationInProgress = false
     
     init(_ habit: Habit) {
         self.habit = habit
         self.title = habit.title
+        self.emoji = habit.emoji
     }
     
     var body: some View {
@@ -32,9 +37,10 @@ struct HabitCard: View {
     private func header() -> some View {
         HStack {
             HStack(spacing: 4) {
-                Text(habit.emoji.isEmpty ? "🎯" : habit.emoji)
+                Text(emoji.isEmpty ? "🎯" : emoji)
                     .font(.system(size: 34, weight: .regular))
                     .frame(width: 38, height: 38)
+                    .scaleEffect(emojiScale)
                     .saturation(habit.emoji.isEmpty ? 0 : 1)
                     .contrast(habit.emoji.isEmpty ? 1.17 : 1)
                     .brightness(
@@ -44,15 +50,35 @@ struct HabitCard: View {
                 Text(title)
                     .font(.headline)
                     .contentTransition(.numericText())
-                    .onChange(of: habit.title) { _, newValue in
-                        Task {
-                            try? await Task.sleep(for: .seconds(0.5))
-                            withAnimation { title = newValue }
-                        }
-                    }
             }
             .foregroundStyle(.accent)
             .onTapGesture { ModalManager.shared.present(.habitInfoSheet(habit)) }
+            .onChange(of: [emoji: habit.emoji, title: habit.title]) { old, new in
+                Task {
+                    guard !isAnimationInProgress else { return }
+                    guard let newEmoji = new[emoji] else { return }
+                    guard let newTitle = new[title] else { return }
+                    
+                    isAnimationInProgress = true
+                    
+                    if title != newTitle {
+                        try? await Task.sleep(for: .seconds(0.45))
+                        withAnimation { title = newTitle }
+                    }
+                    
+                    if emoji != newEmoji {
+                        try? await Task.sleep(for: .seconds(0.55))
+                        
+                        withAnimation(.spring(.bouncy(duration: 0.3))) { emojiScale = 0.2 }
+                        try? await Task.sleep(for: .seconds(0.15))
+                        withAnimation(.spring(.bouncy(duration: 0.3))) { emojiScale = 1.0 }
+                        
+                        emoji = newEmoji
+                    }
+                    
+                    isAnimationInProgress = false
+                }
+            }
             
             Spacer()
             
