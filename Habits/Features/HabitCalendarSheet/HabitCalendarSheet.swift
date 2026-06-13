1308557112855f17b761de2bbdccc2668ef18519
Andrey <244708908+AppleDeveloper101@users.noreply.github.com>
2026-06-13 09:47:28 +0300

    Extract calendarGridCell into HabitCalendarGridCell structure

--- Habits/Features/HabitCalendarSheet/HabitCalendarSheet.swift
+++ Habits/Features/HabitCalendarSheet/HabitCalendarSheet.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import SwiftData
 
-// TODO: Refactor
+// TODO: Refactor, optimize performance if possible
 
 struct HabitCalendarSheet: View {
     
@@ -100,11 +100,12 @@ struct HabitCalendarSheet: View {
                                         placeHolderCell()
                                     }
                                     ForEach(month.monthDaysRange, id: \.self) { dayDate in
-                                        calendarGridCell(
+                                        HabitCalendarGridCell(
                                             date: dayDate,
                                             hasRecord: records.contains { calendar.isDate($0.timestamp, inSameDayAs: dayDate) },
                                             isToday: calendar.isDate(dayDate, inSameDayAs: .startOfToday),
-                                            isDisabled: dayDate > .startOfToday || dayDate < calendar.startOfDay(for: habit.timestamp)
+                                            isDisabled: dayDate > .startOfToday || dayDate < calendar.startOfDay(for: habit.timestamp),
+                                            habit: habit
                                         )
                                     }
                                 }
@@ -164,17 +165,64 @@ struct HabitCalendarSheet: View {
         .fixedSize()
     }
     
-    @ViewBuilder private func calendarGridCell(date: Date, hasRecord: Bool, isToday: Bool, isDisabled: Bool) -> some View {
-        
-        var cellFillColor: Color { hasRecord ? .accent : .clear }
-        var indicatorWidth: CGFloat { hasRecord ? 16 : 6 }
-        var indicatorHeight: CGFloat { hasRecord ? 4 : 6 }
-        var indicatorColor: Color { hasRecord ? .complementary : .accent }
+    private func placeHolderCell() -> some View {
+        Color.clear.frame(width: 44, height: 44)
+    }
+}
+
+struct HabitCalendarGridCell: View {
+    let date: Date
+    let hasRecord: Bool
+    let isToday: Bool
+    let isDisabled: Bool
+    let habit: Habit
+    
+    @State private var isBackgroundFilled: Bool
+    @State private var visualHasRecord: Bool
+    @State private var isAwaitingDelay = false
+    
+    @State private var animationOffset: CGFloat = 0.0
+    @State private var scaleX: CGFloat = 0.0
+    @State private var scaleY: CGFloat = 0.0
+    
+    @State private var textScale: CGFloat = 1.0
+    
+    init(date: Date, hasRecord: Bool, isToday: Bool, isDisabled: Bool, habit: Habit) {
+        self.date = date
+        self.hasRecord = hasRecord
+        self.isToday = isToday
+        self.isDisabled = isDisabled
+        self.habit = habit
+        self._visualHasRecord = State(initialValue: hasRecord)
+        self._isBackgroundFilled = State(initialValue: hasRecord)
+    }
+    
+    var body: some View {
+        let indicatorWidth: CGFloat = visualHasRecord ? 16.0 : 6.0
+        let indicatorHeight: CGFloat = visualHasRecord ? 4.0 : 6.0
+        let cellFillColor: Color = .accent
+        let indicatorColor: Color = isBackgroundFilled ? .complementary : .accent
         
         Text(date.formatted(.dateTime.day(.defaultDigits)))
+            .fixedSize()
             .font(.title3.bold())
+            .scaleEffect(textScale, anchor: .center)
+            .onChange(of: isBackgroundFilled) { _, willFill in
+                if willFill {
+                    Task {
+                        try? await Task.sleep(for: .seconds(isToday ? 0.3 : 0.2))
+                        withAnimation(.spring(duration: 0.2)) {
+                            textScale = 1.15
+                        }
+                        try? await Task.sleep(for: .seconds(0.2))
+                        withAnimation(.spring(duration: 0.2)) {
+                            textScale = 1.0
+                        }
+                    }
+                }
+            }
             .foregroundStyle(
-                hasRecord ? .complementary
+                isBackgroundFilled ? .complementary
                 : isDisabled ? .sheetCalendarGridCellDisabled : .accent
             )
             .overlay(alignment: .bottom) {
@@ -182,27 +230,67 @@ struct HabitCalendarSheet: View {
                     Capsule()
                         .fill(indicatorColor)
                         .frame(width: indicatorWidth, height: indicatorHeight)
-                        .animation(.spring(duration: 0.4, bounce: hasRecord ? 0.5 : 0.35), value: indicatorWidth)
+                        .animation(.spring(duration: 0.4, bounce: visualHasRecord ? 0.5 : 0.4), value: indicatorWidth)
                         .offset(y: indicatorHeight + 0.5)
                 }
             }
             .frame(width: 44, height: 44)
             .background {
-                if hasRecord {
-                    RoundedRectangle(cornerRadius: 44 * 0.34375)
-                        .foregroundStyle(cellFillColor)
-                        .animation(.smooth(duration: 0.425), value: cellFillColor)
-                        .transition(.asymmetric(insertion: .scale, removal: .scale.combined(with: .opacity)))
-                }
+                RoundedRectangle(cornerRadius: 44 * 0.34375)
+                    .foregroundStyle(cellFillColor)
+                    .scaleEffect(x: scaleX, y: scaleY, anchor: .bottom)
+                    .animation(.smooth, value: isBackgroundFilled)
+                    .offset(y: animationOffset)
+                    .onChange(of: isBackgroundFilled) { _, willFill in
+                        Task {
+                            withAnimation(isBackgroundFilled ? .spring(.bouncy) : .smooth) {
+                                scaleX = isBackgroundFilled ? 1.0 : 0.0
+                                scaleY = scaleX
+                            }
+                            if isToday {
+                                withAnimation(.spring) {
+                                    animationOffset = willFill ? -20 : 20
+                                }
+                                try? await Task.sleep(for: .seconds(0.2))
+                                withAnimation(.spring(duration: 0.3, bounce: willFill ? 0.5 : 0.3)) {
+                                    animationOffset = willFill ? 0 : -4
+                                }
+                            }
+                        }
+                    }
+                    .onAppear {
+                        animationOffset = isToday && !hasRecord ? -4 : 0
+                        scaleX = isBackgroundFilled ? 1.0 : 0.0
+                        scaleY = scaleX
+                    }
             }
-            .animation(.smooth, value: hasRecord)
+            .animation(.smooth, value: visualHasRecord)
             .onTapGesture {
                 guard !isDisabled else { return }
+                guard !isAwaitingDelay else { return }
+                
                 DataManager.shared.toggleRecord(for: habit, on: date)
+                
+                isAwaitingDelay = true
+                
+                Task {
+                    withAnimation(.smooth(duration: 0.35)) {
+                        isBackgroundFilled.toggle()
+                    }
+                    
+                    try? await Task.sleep(for: .seconds(0.35))
+                    
+                    withAnimation(.smooth) {
+                        visualHasRecord.toggle()
+                    } completion: {
+                        isAwaitingDelay = false
+                    }
+                }
             }
-    }
-    
-    private func placeHolderCell() -> some View {
-        Color.clear.frame(width: 44, height: 44)
+            .onChange(of: hasRecord) { _, newValue in
+                if !isAwaitingDelay {
+                    visualHasRecord = newValue
+                }
+            }
     }
 }
