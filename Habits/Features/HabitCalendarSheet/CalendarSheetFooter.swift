bfa1296f0a7c186e194446fc443c7bbe5e16a8a0
Andrey <244708908+mxckane@users.noreply.github.com>
2026-07-21 20:59:27 +0300

    Add view template for CalendarSheetFooter

---
+++ Habits/Features/HabitCalendarSheet/CalendarSheetFooter.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarSheetFooter.swift
+//  Habits
+//
+//  Created by Andrey on 22/07/2026.
+//
+
+import SwiftUI
+
+struct CalendarSheetFooter: View {
+    
+    var body: some View {
+        
+    }
+    
+}
