44ea4f21b993432c2c4c65b5ffd4f09ca4024d63
Andrey <244708908+mxckane@users.noreply.github.com>
2026-07-22 21:11:25 +0300

    Add view template for MonthGridStats

---
+++ Habits/Features/HabitCard/MonthGridStats.swift
@@ -0,0 +1,16 @@
+//
+//  MonthGridStats.swift
+//  Habits
+//
+//  Created by Andrey on 22/07/2026.
+//
+
+import SwiftUI
+
+struct MonthGridStats: View {
+    
+    var body: some View {
+        
+    }
+    
+}
