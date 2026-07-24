9e446dfaa67d1a64ab6c72cbc036fba53b6aa80d
Andrey <244708908+mxckane@users.noreply.github.com>
2026-07-24 19:18:02 +0300

    Implement metricBlock view builder in MonthGridStats

--- Habits/Features/HabitCard/MonthGridStats.swift
+++ Habits/Features/HabitCard/MonthGridStats.swift
@@ -13,4 +13,39 @@ struct MonthGridStats: View {
         
     }
     
+    @ViewBuilder private func metricBlock(
+        title: String? = nil,
+        value: Double,
+        valueTitle: String? = nil,
+        imageSystemName: String? = nil,
+        valueToImageSpacing: CGFloat = 2.0
+    ) -> some View {
+        VStack(spacing: 2.0) {
+            if let title {
+                Text(title)
+                    .fontWeight(.semibold)
+                    .font(.system(size: 12))
+                    .foregroundStyle(.labelPrimary)
+            }
+            HStack(spacing: valueToImageSpacing) {
+                HStack(spacing: .zero) {
+                    Text(value.description) // TODO: Formatting for decimal part
+                    if let valueTitle {
+                        Text(valueTitle)
+                    }
+                }
+                if let imageSystemName {
+                    Image(systemName: imageSystemName)
+                }
+            }
+            .fontWeight(.semibold)
+            .font(.system(size: 14))
+            .foregroundStyle(.sheetCalendarFooterStatsPrimary)
+        }
+    }
+    
+}
+
+#Preview {
+    MonthGridStats()
 }
