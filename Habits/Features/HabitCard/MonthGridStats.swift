075be542b3c60a8d1929711100cc0329e7937284
Andrey <244708908+mxckane@users.noreply.github.com>
2026-07-26 20:11:48 +0300

    Add implementation for MonthGridStats

--- Habits/Features/HabitCard/MonthGridStats.swift
+++ Habits/Features/HabitCard/MonthGridStats.swift
@@ -9,32 +9,38 @@ import SwiftUI
 
 struct MonthGridStats: View {
     
-    var body: some View {
-        
+    @State var metrics: [FooterMetric]
+    
+    private var metricsEnumerated: Array<(offset: Int, element: FooterMetric)> {
+        Array(metrics.enumerated())
     }
     
-    @ViewBuilder private func metricBlock(
-        title: String? = nil,
-        value: Double,
-        valueTitle: String? = nil,
-        imageSystemName: String? = nil,
-        valueToImageSpacing: CGFloat = 2.0
-    ) -> some View {
+    var body: some View {
+        HStack {
+            ForEach(metricsEnumerated, id: \.element.id) { index, metric in
+                metricBlock(metric)
+                if index != metricsEnumerated.indices.last { Spacer() }
+            }
+        }
+    }
+    
+    @ViewBuilder private func metricBlock(_ metric: FooterMetric) -> some View {
         VStack(spacing: 2.0) {
-            if let title {
+            if let title = metric.title {
                 Text(title)
                     .fontWeight(.semibold)
                     .font(.system(size: 12))
                     .foregroundStyle(.labelPrimary)
             }
-            HStack(spacing: valueToImageSpacing) {
+            HStack(spacing: metric.valueToImageSpacing) {
                 HStack(spacing: .zero) {
-                    Text(value.description) // TODO: Formatting for decimal part
-                    if let valueTitle {
+                    Text(metric.value.description) // TODO: Formatting for decimal part
+                    // TODO: Natural spacing between metric value and title; concatenation?
+                    if let valueTitle = metric.valueTitle {
                         Text(valueTitle)
                     }
                 }
-                if let imageSystemName {
+                if let imageSystemName = metric.imageSystemName {
                     Image(systemName: imageSystemName)
                 }
             }
@@ -46,6 +52,24 @@ struct MonthGridStats: View {
     
 }
 
+// TODO: Better example
+
 #Preview {
-    MonthGridStats()
+    MonthGridStats(
+        metrics: [
+            FooterMetric(
+                title: "Test",
+                value: 47.43,
+                imageSystemName: "percent",
+                valueToImageSpacing: 0.0
+            ),
+            FooterMetric(
+                title: "Tset",
+                value: 12,
+                valueTitle: "Cirtem",
+                imageSystemName: "leaf",
+            ),
+        ]
+    )
+    .padding(.horizontal, 48)
 }
