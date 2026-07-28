8bef833d336b0cfce1e1256bc1141bc9f77e7722
Andrey <244708908+mxckane@users.noreply.github.com>
2026-07-28 11:55:34 +0300

    Add new properties for FooterMetric model

--- Habits/Models/FooterMetric.swift
+++ Habits/Models/FooterMetric.swift
@@ -9,9 +9,14 @@ import Foundation
 
 struct FooterMetric: Identifiable {
     let id: UUID = UUID()
-    var title: String? = nil
-    var value: Double
+    
+    var header: String? = nil
+    var value: Double? = nil
     var valueTitle: String? = nil
     var imageSystemName: String? = nil
+    
+    @NonNegative var valueDecimalPlaces: Int = 2
+    var formattedValue: String? { value?.formatted(.number.precision(.fractionLength(0...valueDecimalPlaces))) }
+    
     var valueToImageSpacing: CGFloat = 2.0
 }
