271b61ba780e2d0caa1413c826b49f913ad73fd5
Andrey <244708908+mxckane@users.noreply.github.com>
2026-07-29 15:10:56 +0300

    Complete initial implementation of CalendarSheetFooter

--- Habits/Features/HabitCalendarSheet/CalendarSheetFooter.swift
+++ Habits/Features/HabitCalendarSheet/CalendarSheetFooter.swift
@@ -9,8 +9,72 @@ import SwiftUI
 
 struct CalendarSheetFooter: View {
     
-    var body: some View {
-        
+    let metrics: [FooterMetric] // TODO: Check for reactive updates later
+    
+    private var metricsEnumerated: Array<(offset: Int, element: FooterMetric)> {
+        Array(metrics.enumerated())
     }
     
+    init(_ metrics: [FooterMetric]) {
+        self.metrics = metrics
+    }
+    
+    var body: some View {
+        HStack {
+            ForEach(metricsEnumerated, id: \.element.id) { index, metric in
+                metricView(metric)
+                if index != metricsEnumerated.indices.last { Spacer() }
+            }
+        }
+    }
+    
+    @ViewBuilder private func metricView(_ metric: FooterMetric) -> some View {
+        VStack(spacing: 2.0) {
+            if let header = metric.header {
+                Text(header)
+                    .font(.system(size: 12, weight: .semibold))
+                    .foregroundStyle(.labelPrimary)
+            }
+            HStack(spacing: metric.valueToImageSpacing) {
+                if let valueWithTitle = metric.formattedValueWithTitle {
+                    Text(valueWithTitle) // TODO: Consider adding centering by value title
+                }
+                if let imageSystemName = metric.imageSystemName {
+                    Image(systemName: imageSystemName)
+                }
+            }
+            .font(.system(size: 14, weight: .semibold))
+            .foregroundStyle(.sheetCalendarFooterStatsPrimary)
+        }
+    }
+    
+}
+
+#Preview {
+    CalendarSheetFooter([
+        FooterMetric(
+            header: "Counter",
+            value: 147,
+            valueTitle: "Carrots",
+            imageSystemName: "carrot",
+        ),
+        FooterMetric(
+            header: "Percentage",
+            value: 147/255*100,
+            imageSystemName: "percent",
+            valueDecimalPlaces: 1,
+            valueToImageSpacing: 1.0
+        ),
+        FooterMetric(
+            header: "Category",
+            valueTitle: "Flowers",
+            imageSystemName: "camera.macro.circle",
+            valueDecimalPlaces: 5,
+        ),
+        FooterMetric(
+            header: "Plain Metric",
+            value: 609
+        )
+    ])
+    .padding(.horizontal)
 }
--- Habits/Features/HabitCard/MonthGridStats.swift
+++ Habits/Features/HabitCard/MonthGridStats.swift
@@ -9,67 +9,8 @@ import SwiftUI
 
 struct MonthGridStats: View {
     
-    @State var metrics: [FooterMetric]
-    
-    private var metricsEnumerated: Array<(offset: Int, element: FooterMetric)> {
-        Array(metrics.enumerated())
-    }
-    
     var body: some View {
-        HStack {
-            ForEach(metricsEnumerated, id: \.element.id) { index, metric in
-                metricBlock(metric)
-                if index != metricsEnumerated.indices.last { Spacer() }
-            }
-        }
+        
     }
     
-    @ViewBuilder private func metricBlock(_ metric: FooterMetric) -> some View {
-        VStack(spacing: 2.0) {
-            if let title = metric.title {
-                Text(title)
-                    .fontWeight(.semibold)
-                    .font(.system(size: 12))
-                    .foregroundStyle(.labelPrimary)
-            }
-            HStack(spacing: metric.valueToImageSpacing) {
-                HStack(spacing: .zero) {
-                    Text(metric.value.description) // TODO: Formatting for decimal part
-                    // TODO: Natural spacing between metric value and title; concatenation?
-                    if let valueTitle = metric.valueTitle {
-                        Text(valueTitle)
-                    }
-                }
-                if let imageSystemName = metric.imageSystemName {
-                    Image(systemName: imageSystemName)
-                }
-            }
-            .fontWeight(.semibold)
-            .font(.system(size: 14))
-            .foregroundStyle(.sheetCalendarFooterStatsPrimary)
-        }
-    }
-    
-}
-
-// TODO: Better example
-
-#Preview {
-    MonthGridStats(
-        metrics: [
-            FooterMetric(
-                title: "Test",
-                value: 47.43,
-                imageSystemName: "percent",
-                valueToImageSpacing: 0.0
-            ),
-            FooterMetric(
-                title: "Tset",
-                value: 12,
-                valueTitle: "Cirtem",
-                imageSystemName: "leaf",
-            ),
-        ]
-    )
-    .padding(.horizontal, 48)
 }
