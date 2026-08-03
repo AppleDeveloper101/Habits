40a198b79fc16b533b1221b72bc8d708c7771123
Andrey <244708908+mxckane@users.noreply.github.com>
2026-08-03 21:07:19 +0300

    Draft month grid stats layout

--- Habits/Features/HabitCard/MonthGridStats.swift
+++ Habits/Features/HabitCard/MonthGridStats.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct MonthGridStats: View {
     
+    private let monthGridSpacing: CGFloat = 8
+    
     var body: some View {
-        
+        HStack(spacing: monthGridSpacing) {
+            ForEach(0..<3) { month in
+                monthGrid()
+            }
+        }
     }
     
+    @ViewBuilder private func monthGrid() -> some View {
+        Grid(alignment: .center, horizontalSpacing: 2, verticalSpacing: 2) {
+            ForEach(0..<7) { _ in
+                GridRow(alignment: .center) {
+                    ForEach(0..<4) { _ in
+                        Rectangle()
+                            .aspectRatio(1, contentMode: .fit)
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    MonthGridStats()
+        .padding(12)
+        .background {
+            DefaultStyleShape(RoundedRectangle(cornerRadius: 24))
+        }
+        .padding()
+}
 }
