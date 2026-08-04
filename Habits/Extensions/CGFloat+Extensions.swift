12335ecc9622ad2c7cdeb1a7ba1b6534d59c6af8
Andrey <244708908+mxckane@users.noreply.github.com>
2026-08-04 20:47:25 +0300

    Add markCornerRadiusCoefficient global constant

--- Habits/Extensions/CGFloat+Extensions.swift
+++ Habits/Extensions/CGFloat+Extensions.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+let markCornerRadiusCoefficient: CGFloat = 0.34375
+
 extension CGFloat {
     static var displayCornerRadius: CGFloat {
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return 0 }
