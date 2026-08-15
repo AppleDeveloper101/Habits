afbf0f50f8661d5eaeabf5d9e846881fea5edda4
Andrey <244708908+mxckane@users.noreply.github.com>
2026-08-15 20:38:23 +0300

    Add configurable variations of Mark

--- Habits/Views/Mark.swift
+++ Habits/Views/Mark.swift
@@ -8,22 +8,41 @@
 import SwiftUI
 
 struct Mark: View {
+    
+    let hasRecord: Bool
+    let isToday: Bool
+    
+    var scaleFactor: CGFloat { hasRecord ? 1.0 : isToday ? 0.5 : 0.25 }
+    
+    init(hasRecord: Bool = false, isToday: Bool = false) {
+        self.hasRecord = hasRecord
+        self.isToday = isToday
+    }
+    
     var body: some View {
         GeometryReader { proxy in
             let cornerRadius = min(proxy.size.width, proxy.size.height) * markCornerRadiusCoefficient
             RoundedRectangle(cornerRadius: cornerRadius)
                 .fill(
                     LinearGradient(
-                        colors: [.weekRowCellStart, .weekRowCellEnd],
+                        colors: hasRecord || isToday ? [.weekRowCellStart, .weekRowCellEnd] : [.weekRowEmptyCell],
                         startPoint: .top,
                         endPoint: .bottom
                     )
                 )
+                .frame(width: proxy.size.width * scaleFactor, height: proxy.size.height * scaleFactor)
+                .position(x: proxy.size.width / 2.0, y: proxy.size.height / 2.0)
         }
         .aspectRatio(1, contentMode: .fit)
     }
+    
 }
 
 #Preview {
-    Mark()
+    VStackLayout(spacing: 2) {
+        Mark(hasRecord: false)
+        Mark(hasRecord: true)
+        Mark(hasRecord: false, isToday: true)
+    }
+    .frame(height: 128)
 }
