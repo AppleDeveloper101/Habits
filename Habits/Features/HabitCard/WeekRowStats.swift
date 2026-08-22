9baee52d35e664cf5d6d740e5d4a7baca16ba625
Andrey <244708908+mxckane@users.noreply.github.com>
2026-08-22 20:33:02 +0300

    Refactoring changes in WeekRowStats

--- Habits/Features/HabitCard/WeekRowStats.swift
+++ Habits/Features/HabitCard/WeekRowStats.swift
@@ -8,98 +8,65 @@
 import SwiftUI
 import SwiftData
 
+// MARK: Try using computed property Habit.records instead of query
+
 struct WeekRowStats: View {
     
-    @Query private var records: [Record]
     private let habit: Habit
     
-    @State private var markWidth: CGFloat = .zero
-    @State private var calculatedCornerRadius: CGFloat = .zero
+    @Query private var records: [Record]
     
-    private let cornerRadiusMultiplier = 0.34375
-    
-    private let calendar = Calendar.current
-    
-    private var datesRange: [Date] {
+    private var datesRange: [Date] = {
         var dates: [Date] = []
-        let startOfCurrentWeek = calendar.dateInterval(of: .weekOfYear, for: .now)!.start
-        let weekRowLastDate = calendar.date(byAdding: .day, value: 6, to: startOfCurrentWeek)!
+        let startOfCurrentWeek = Calendar.current.dateInterval(of: .weekOfYear, for: .now)!.start
+        let weekRowLastDate = Calendar.current.date(byAdding: .day, value: 6, to: startOfCurrentWeek)!
         
         (0..<10).reversed().forEach { index in
-            let dateToAdd = calendar.date(byAdding: .day, value: -index, to: weekRowLastDate)!
+            let dateToAdd = Calendar.current.date(byAdding: .day, value: -index, to: weekRowLastDate)!
             dates.append(dateToAdd)
         }
         
         return dates
-    }
+    }()
     
     init(habit: Habit) {
         self.habit = habit
         
         let fetchedRecordsHabitID = habit.persistentModelID
+        let predicate = #Predicate<Record> { $0.habit?.persistentModelID == fetchedRecordsHabitID }
         
-        let predicate = #Predicate<Record> {
-            $0.habit?.persistentModelID == fetchedRecordsHabitID
-        }
-        
-        self._records = Query(filter: predicate, sort: \.timestamp)
+        _records = Query(filter: predicate, sort: \.timestamp)
     }
     
     var body: some View {
         HStack(spacing: 8) {
-            ForEach(Array(datesRange.enumerated()), id: \.offset) { index, date in
+            ForEach(
+                Array(datesRange.enumerated()), id: \.offset
+            ) { index, date in
                 WeekRowCell(
                     date: date,
-                    hasRecord: records.contains { calendar.isDate($0.timestamp, equalTo: date, toGranularity: .day) },
-                    isToday: date.isToday,
-                    symbol: date.formatted(.dateTime.weekday(.narrow))
+                    hasRecord: records.contains { Calendar.current.isDate($0.timestamp, equalTo: date, toGranularity: .day) },
+                    isToday: date.isToday
                 )
-                if index == 2 { separatorColumn() }
+                if index == 2 {
+                    separatorColumn()
+                }
             }
         }
     }
     
-    @ViewBuilder private func WeekRowCell(date: Date, hasRecord: Bool, isToday: Bool, symbol: String) -> some View {
+    @ViewBuilder private func WeekRowCell(date: Date, hasRecord: Bool, isToday: Bool) -> some View {
         
-        let sizeRatio: CGFloat = {
-            switch (hasRecord, isToday) {
-            case (true, _): return 1.0
-            case (false, true): return 0.5
-            case (false, false): return 0.25
-            }
-        }()
+        let state: Mark.State = hasRecord ? .checked : isToday ? .today : .unchecked
+        let symbol = date.formatted(.dateTime.weekday(.narrow))
         
         VStack(spacing: 4) {
-            GeometryReader { geo in
-                RoundedRectangle(
-                    cornerRadius: hasRecord
-                    ? geo.size.width * cornerRadiusMultiplier
-                    : geo.size.width * sizeRatio / 2
-                )
-                .fill(
-                    LinearGradient(
-                        colors: hasRecord || isToday ? [.weekRowCellStart, .weekRowCellEnd] : [.weekRowEmptyCell],
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                )
-                .frame(
-                    width: geo.size.width * sizeRatio,
-                    height: geo.size.width * sizeRatio
-                )
-                .position(
-                    x: geo.size.width / 2,
-                    y: geo.size.width / 2
-                )
-            }
-            .aspectRatio(1, contentMode: .fit)
-            
+            Mark(state: state)
             Text(symbol)
                 .frame(height: 14)
                 .foregroundStyle(.accent)
                 .font(.system(size: 10, weight: .bold))
         }
-        .padding(.top, 12)
         .contentShape(.rect)
         .onTapGesture {
             ModalManager.shared.present(.habitCalendarSheet(habit, date))
@@ -116,6 +83,5 @@ struct WeekRowStats: View {
                 .fill(.weekRowSeparatorSecondary)
                 .frame(width: 1.5, height: 14)
         }
-        .padding(.top, 12)
     }
 }
