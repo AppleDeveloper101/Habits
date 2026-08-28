81cb0425fa2b254a92efe63d9b04b9c0be176266
Andrey <244708908+mxckane@users.noreply.github.com>
2026-08-28 14:18:39 +0300

    Add weekdays and month names to new MonthGridStats draft

--- Habits/Features/HabitCard/MonthGridStats.swift
+++ Habits/Features/HabitCard/MonthGridStats.swift
@@ -16,6 +16,9 @@ struct MonthGridStats: View {
     private let gridGapsCount: Int
     private let cellGapsCount: Int
     
+    private let monthNameToGridSpacing: CGFloat = 4.0
+    private let weekdaysToGridsSpacing: CGFloat = 4.0
+    private let weekdaysSymbolWidth: CGFloat = 16.0
     private let cellSpacing: CGFloat = 2.0
     private let gridSpacing: CGFloat = 8.0
     private let spacingTotal: CGFloat
@@ -38,29 +41,60 @@ struct MonthGridStats: View {
     }
     
     var body: some View {
-        HStack(spacing: gridSpacing) {
-            ForEach(gridModels) { model in
-                grid(model: model, cellSize: cellSize)
-                    .border(.blue.opacity(1/2))
+        HStack(alignment: .bottom, spacing: weekdaysToGridsSpacing) {
+            weekdaysColumn(cellSize: cellSize)
+            HStack(spacing: gridSpacing) {
+                ForEach(gridModels) { model in
+                    grid(model: model, cellSize: cellSize)
+                        .border(.blue.opacity(1/2))
+                }
             }
+            .frame(maxWidth: .infinity)
+            .readSize(.horizontal, into: $gridsContainerWidth)
+            .border(.green.opacity(1/2))
         }
-        .frame(maxWidth: .infinity)
-        .readSize(.horizontal, into: $gridsContainerWidth)
-        .border(.green.opacity(1/2))
     }
     
     @ViewBuilder private func grid(model: MonthGridViewModel, cellSize: CGFloat) -> some View {
-        Grid(horizontalSpacing: cellSpacing, verticalSpacing: cellSpacing) {
-            ForEach(0..<7) { row in
-                GridRow {
-                    ForEach(0..<model.columnsCount, id: \.self) { column in
-                        Mark()
-                            .frame(width: cellSize)
-                            .border(.black.opacity(1/2))
+        VStack(alignment: .leading, spacing: monthNameToGridSpacing) {
+            Text(model.date.formatted(.dateTime.month(.wide)))
+                .foregroundStyle(.accent)
+                .font(.footnote)
+                .bold()
+                .lineLimit(1)
+                .frame(height: 18.0)
+            
+            Grid(horizontalSpacing: cellSpacing, verticalSpacing: cellSpacing) {
+                ForEach(0..<7) { row in
+                    GridRow {
+                        ForEach(0..<model.columnsCount, id: \.self) { column in
+                            Mark()
+                                .frame(width: cellSize)
+                                .border(.black.opacity(1/2))
+                        }
                     }
                 }
             }
         }
+    }
+    
+    @ViewBuilder private func weekdaysColumn(cellSize: CGFloat) -> some View {
+        HStack(spacing: 2.0) {
+            VStack(spacing: cellSpacing) {
+                ForEach(["m", "t", "w", "t", "f", "s", "s"].map { $0.uppercased() }, id: \.self) { symbol in
+                    Text(symbol)
+                        .foregroundStyle(.accent)
+                        .font(.system(size: 9.0))
+                        .fontWeight(.semibold)
+                        .frame(width: weekdaysSymbolWidth, height: cellSize)
+                        .border(.pink.opacity(1/2))
+                }
+            }
+            Capsule()
+                .foregroundStyle(.monthGridStatsWeekdaysColumnSeparator)
+                .frame(width: 1.0, height: cellSize * 7 + cellSpacing * 6)
+        }
+        .border(.green.opacity(1/2))
     }
     
 }
@@ -68,10 +102,12 @@ struct MonthGridStats: View {
 private struct MonthGridViewModel: Identifiable {
     
     let id: UUID
+    let date: Date
     let columnsCount: Int
     
     init() {
         self.id = UUID()
+        self.date = .now
         self.columnsCount = .random(in: 1...6)
     }
     
